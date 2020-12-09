2c7dfb6ab0434e9ebfa78d3f8651592ac3eb8f70
Kevin Bevan <kevin.bevan@cdsm.co.uk>
2020-12-09 03:29:54 +0000

    WIP - dark theme/mode

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,17 +1,31 @@
 import Link from "next/link"
-import { Logo } from "../icons/svgr"
+// import { Logo } from "../icons/svgr"
+import { useTheme } from "next-themes"
 import styles from "./header.module.scss"
 
 export function Header() {
+	const { theme, setTheme } = useTheme()
+
 	return (
-		<div className={styles.header}>
-			<Logo size={35} />
-			<h2>
-				<Link href='/'>
-					<a>Kevin's Blog</a>
-				</Link>
-				.
-			</h2>
-		</div>
+		<header className={styles.header}>
+			<div className={styles.menu}>
+				{/* <Logo size={35} /> */}
+				<h2>
+					<Link href='/'>
+						<a>Kevin's Blog</a>
+					</Link>
+					.
+				</h2>
+				<ul>
+					<li>Latest</li>
+					<li>Collection</li>
+					<li>Categories</li>
+				</ul>
+			</div>
+			<div className={styles.buttons}>
+				<button onClick={() => setTheme("light")}>Light Mode</button>
+				<button onClick={() => setTheme("dark")}>Dark Mode</button>
+			</div>
+		</header>
 	)
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
-import '../styles/index.css'
+import { ThemeProvider } from "next-themes"
+import "normalize.css"
+import "../styles/index.css"
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return (
+		<ThemeProvider>
+			<Component {...pageProps} />
+		</ThemeProvider>
+	)
 }
 
 export default MyApp
