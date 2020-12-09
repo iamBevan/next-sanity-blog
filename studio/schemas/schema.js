a1b44404a4cb49fa0c486017d8a4fff150670d53
Kevin Bevan <kevin.bevan@cdsm.co.uk>
2020-12-09 04:00:53 +0000

    test schema ts to js

--- studio/schemas/schema.js
+++ studio/schemas/schema.js
@@ -5,7 +5,7 @@ import createSchema from "part:@sanity/base/schema-creator"
 import schemaTypes from "all:part:@sanity/base/schema-type"
 
 // We import object and document schemas
-import { Author, BlockContent, Category, Comment, Post } from "./"
+import { Author, BlockContent, Category, Comment, Post } from "."
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
