a849cb930d3ccc103a0d0d357438ca31aa1c8a5a
Kevin Bevan <kevin.bevan@cdsm.co.uk>
2020-12-10 02:20:20 +0000

    test colours hydration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
-import { ThemeProvider } from "next-themes"
+import { ThemeProvider, useTheme } from "next-themes"
+import { useEffect, useState } from "react"
 // import "normalize.css"
 import "../styles/index.css"
 
 function MyApp({ Component, pageProps }) {
+	const [mounted, setMounted] = useState(false)
+	const { theme, setTheme } = useTheme()
+
+	// When mounted on client, now we can show the UI
+	useEffect(() => setMounted(true), [])
+
+	if (!mounted) return null
+
 	return (
 		<ThemeProvider>
 			<Component {...pageProps} />
