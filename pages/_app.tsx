6f70b85268da6e79ec4b3e30c6fe173f677d683f
Kevin Bevan <kevin.bevan@cdsm.co.uk>
2020-12-10 02:41:44 +0000

    test new hook for handling dark mode

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,8 @@
-import { ThemeProvider, useTheme } from "next-themes"
-import { useEffect, useState } from "react"
+import { ThemeProvider } from "next-themes"
 // import "normalize.css"
 import "../styles/index.css"
 
 function MyApp({ Component, pageProps }) {
-	const [mounted, setMounted] = useState(false)
-	const { theme, setTheme } = useTheme()
-
-	// When mounted on client, now we can show the UI
-	useEffect(() => setMounted(true), [])
-
-	if (!mounted) return null
-
 	return (
 		<ThemeProvider>
 			<Component {...pageProps} />
