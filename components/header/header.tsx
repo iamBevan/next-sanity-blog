81df0a33a830b7e895f5ff72efc887d9662bcbc0
Kevin Bevan <kevin.bevan@cdsm.co.uk>
2020-12-11 03:42:14 +0000

    🚧 refactor

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,10 +1,6 @@
 import Link from "next/link"
-// import { Logo } from "../icons/svgr"
 import { useTheme } from "next-themes"
 import styles from "./header.module.scss"
-import Toggle from "react-toggle"
-import { useState } from "react"
-import { Moon } from "../icons/svgr"
 import { ToggleButton } from "../toggle-button/toggle-button"
 
 export function Header() {
