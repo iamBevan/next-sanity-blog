994dfad4f03e69d35ebd55176766033816b0d604
Kevin Bevan <kevin.bevan@cdsm.co.uk>
2020-12-12 03:51:24 +0000

    🧪dark mode button

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -2,16 +2,9 @@ import Link from "next/link"
 import { useTheme } from "next-themes"
 import styles from "./header.module.scss"
 import { ToggleButton } from "../toggle-button/toggle-button"
-import Toggle from "react-toggle"
-import { useEffect } from "react"
-import "react-toggle/style.css"
 
 export function Header() {
 	const { theme, setTheme } = useTheme()
-
-	useEffect(() => {
-		setTheme("light")
-	}, [])
 
 	return (
 		<header className={styles.header}>
@@ -29,22 +22,8 @@ export function Header() {
 				</ul>
 			</div>
 			<div className={styles.buttons}>
-				{/* <ToggleButton
+				<ToggleButton
 					checked={theme === "light" ? false : true}
-					onChange={() =>
-						// setTheme(theme === "light" ? "dark" : "light")
-						console.log("dog")
-					}
-					key={1}
-				/> */}
-				<Toggle
-					// defaultChecked={theme === "light" ? false : true}
-					aria-label='Toggle theme'
-					// className='custom-classname'
-					icons={{
-						checked: null,
-						unchecked: null,
-					}}
 					onChange={() =>
 						setTheme(theme === "light" ? "dark" : "light")
 					}
